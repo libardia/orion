a44ff1072d442225d30ae3f9b4a3d2696a5f793f
Anthony Libardi <libardi42@gmail.com>
2024-11-27 12:01:33 -0500

    restructure again

--- scenes/title/main-global.gd
+++ scenes/title/main-global.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 
+func _ready() -> void:
+    UICommon.init(self)
+
+
 func _input(event: InputEvent) -> void:
     if event.is_action_pressed("debug-quit") and OS.is_debug_build():
         get_tree().quit()
